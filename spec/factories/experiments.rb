d44f7559fedb8107a5c636ce36bdc18a36815aaf
aivic-unigedesk <ali.ivic88@gmail.com>
2015-02-18 18:37:08 +0100

    Nothin

---
+++ spec/factories/experiments.rb
@@ -0,0 +1,26 @@
+# spec/factories/experiments.rb
+require 'faker'
+
+FactoryGirl.define do
+  factory :contact do |f|
+    # f.firstname { Faker::Name.first_name }
+    f.title "Lulea meeting experiment"
+    f.description "In order to test the IoT Lab this service show the potential usage of smartphone crowdsource"
+    f.start_datetime "18/02/2015 02:00 pm"
+    f.end_datetime "20/02/2015 02:00 pm"
+    f.participation ""
+    f.voters "24"
+    f.ranking "1"
+    f.interaction ""
+    f.status "finished"
+    f.location "switzerland"
+    f.radius "10"
+    f.minimum_age "10"
+    f.maximum_age "20"
+    f.gender "m"
+    f.employment_sector "IT"
+    f.employment_status "full-time"
+    f.camera "sdfasdf"
+    f.accelerometer "23"
+  end
+end
