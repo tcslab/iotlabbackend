ec17f56e2e6e8843cd1f465a9cd8ecc5b19e14e8
aivic-unigedesk <ali.ivic88@gmail.com>
2015-02-19 14:55:10 +0100

    merge from another branch to support json operations

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   resources :experiments, :path => '/'
 
   resources :experiments do
-    # collection do
-    #   get 'getnodes'
-    #   get 'getlightnodes'
+    collection do
+      get 'index'
+      get 'show'
     #   get 'gettemperaturenodes'
-    # end
+    end
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
