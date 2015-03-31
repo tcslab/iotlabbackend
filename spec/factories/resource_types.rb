5d78d6c3d5ecf75ee2503d354a2765d654f3c765
aivic-unigedesk <ali.ivic88@gmail.com>
2015-03-31 17:17:22 +0200

    Writes Rspec for Resource_Type Model

--- spec/factories/resource_types.rb
+++ spec/factories/resource_types.rb
@@ -1,9 +1,10 @@
+require 'faker'
 FactoryGirl.define do
   factory :resource_type do
-    name "MyString"
-    restype "MyString"
-    notes "MyText"
-    interface_id nil
-    data_types_id ""
+    name { Faker::Name.first_name }
+    # restype "MyString"
+    # notes "MyText"
+    # interface_id nil
+    # data_types_id ""
   end
 end
--- spec/models/resource_type_spec.rb
+++ spec/models/resource_type_spec.rb
@@ -2,9 +2,6 @@ require 'rails_helper'
 
 RSpec.describe ResourceType, type: :model do
   it "is invalid without a name" do
-    expect(FactoryGirl.build(:discovery_source, name:nil)).not_to be_nil
-  end
-  it "is invalid without a url" do
-    expect(FactoryGirl.build(:discovery_source, url:nil)).not_to be_nil
+    expect(FactoryGirl.build(:resource_type, name:nil)).not_to be_nil
   end
 end
