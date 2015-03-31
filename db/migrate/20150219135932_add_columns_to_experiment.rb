e21e68c0843ec35c2266c24edd57fe34f645df82
aivic-unigedesk <ali.ivic88@gmail.com>
2015-03-31 10:50:00 +0200

    Removes all previous migration in favour of Marios Karagianis db schema

--- db/migrate/20150219135932_add_columns_to_experiment.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToExperiment < ActiveRecord::Migration
-  def change
-    add_column :experiments, :location_sensor, :string
-    add_column :experiments, :magnetometer, :string
-  end
-end
