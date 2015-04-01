ac04a0e3f969c5fd12c0ccfddc908a4c66ef4ba0
aivic-unigedesk <ali.ivic88@gmail.com>
2015-04-01 19:48:52 +0200

    Cleanes up the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,26 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# DataType.create([{ name: 'b (boolean)' , plainformat: 'xsd:boolean' , senml: 'Boolean Value (v)' } ,
-#                  { name: 's (string)'  , plainformat: 'xsd:string'  , senml: 'String Value (sv)' } ,
-#                  { name: 'e (enum)'    , plainformat: 'xsd:integer' , senml: 'Value (v)' } ,
-#                  { name: 'i (integer)' , plainformat: 'xsd:integer' , senml: 'Value (v)' } ,
-#                  { name: 'd (decimal)' , plainformat: 'xsd:decimal' , senml: 'Value (v)' } ,
-#                  { name: 't (time)' , plainformat: 'xsd:time' , senml: 'Time (t)' },
-#                  { name: 'f (float)' , plainformat: 'xsd:float' , senml: 'Float Value (f)' },
-#                  { name: 'o (opaque)' , plainformat: 'xsd:opaque' , senml: 'Opaque Value (o)' }
-# ])
+DataType.create([{ name: 'b (boolean)' , plainformat: 'xsd:boolean' , senml: 'Boolean Value (v)' } ,
+                 { name: 's (string)'  , plainformat: 'xsd:string'  , senml: 'String Value (sv)' } ,
+                 { name: 'e (enum)'    , plainformat: 'xsd:integer' , senml: 'Value (v)' } ,
+                 { name: 'i (integer)' , plainformat: 'xsd:integer' , senml: 'Value (v)' } ,
+                 { name: 'd (decimal)' , plainformat: 'xsd:decimal' , senml: 'Value (v)' } ,
+                 { name: 't (time)' , plainformat: 'xsd:time' , senml: 'Time (t)' },
+                 { name: 'f (float)' , plainformat: 'xsd:float' , senml: 'Float Value (f)' },
+                 { name: 'o (opaque)' , plainformat: 'xsd:opaque' , senml: 'Opaque Value (o)' }
+])
 
-# Interface.create([{ name: 'Link List'           , abreviation: 'll'} ,
-#                   { name: 'Batch'               , abreviation: 'b'} ,
-#                   { name: 'Linked Batch'        , abreviation: 'lb'} ,
-#                   { name: 'Sensor'              , abreviation: 's'} ,
-#                   { name: 'Parameter'           , abreviation: 'p'} ,
-#                   { name: 'Read-Only Parameter' , abreviation: 'rp'} ,
-#                   { name: 'Actuator'            , abreviation: 'a'} ,
-#                   { name: 'Binding'             , abreviation: 'bnd'} ,
-#                   { name: 'Undefined'           , abreviation: 'ud'}
-# ])
+Interface.create([{ name: 'Link List'           , abreviation: 'll'} ,
+                  { name: 'Batch'               , abreviation: 'b'} ,
+                  { name: 'Linked Batch'        , abreviation: 'lb'} ,
+                  { name: 'Sensor'              , abreviation: 's'} ,
+                  { name: 'Parameter'           , abreviation: 'p'} ,
+                  { name: 'Read-Only Parameter' , abreviation: 'rp'} ,
+                  { name: 'Actuator'            , abreviation: 'a'} ,
+                  { name: 'Binding'             , abreviation: 'bnd'} ,
+                  { name: 'Undefined'           , abreviation: 'ud'}
+])
 
 
 #Device
