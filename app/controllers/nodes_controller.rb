22210e420f565368071e031ebabd2fef21c51222
aivic-unigedesk <ali.ivic88@gmail.com>
2015-04-16 22:35:52 +0200

    Implements the feature of getting the list of nodes corresponding to the provided latitude and longitude

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -15,11 +15,9 @@ class NodesController < ApplicationController
   # GET /resources/by_location
   # GET /resources/1.json
   def get_nodes_by_location
-    # binding.pry
     @nodes = Node.get_resources_by_location(params[:latitude],params[:longitude])
     # @nodes = Node.get_resources_by_location("46.176388","6.139959")
     # @nodes = Node.get_resources_by_location
-    # @nodes = Node.all
     respond_with(@nodes) do |format|
       format.json { render :json => @nodes.as_json }
     end
